269aa61388a67913cb0075192f70e3c41dba969c
Chris Ware <c.p.ware@hotmail.com>
2025-01-21 21:24:34 -0700

    Marquee done

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -1,21 +1,31 @@
-/* eslint-disable react/jsx-key */
 import styles from "./Marquee.module.css";
 import Heart from "../../../public/icons/heart.svg";
 import Diamond from "../../../public/icons/diamond.svg";
 import Spade from "../../../public/icons/spade.svg";
 import Club from "../../../public/icons/club.svg";
 
-const icons = [
-  <Heart width={30} height={30} />,
-  <Diamond width={30} height={30} />,
-  <Spade width={30} height={30} />,
-  <Club width={30} height={30} />,
-];
-
 export default function Marquee() {
+  const icons = (
+    <div className={styles.iconContainer}>
+      <Heart width={25} height={25} />
+      <Diamond width={25} height={25} />
+      <Spade width={25} height={25} />
+      <Club width={25} height={25} />
+    </div>
+  );
+
   return (
-    <div className={styles.container}>
-      <h2>Marquee</h2>
+    <div className={styles.slider}>
+      <div className={styles.track}>
+        {[...Array(10)].map((_, index) => (
+          <div key={index} className={styles.arrayItems}>
+            {icons}
+            {icons}
+            {icons}
+            {icons}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
